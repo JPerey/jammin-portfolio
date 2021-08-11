9f1f655e3be70e4c771782bcade2393098248707
James Perey <jamjam.perey@gmail.com>
2021-08-11 00:35:39 -0700

    added basic carousel, change after presentation

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,20 +1,82 @@
 import "./HomePage.css";
-import Whitetextbg from "../images/whitetextbg.png";
-import Whitetextbg2 from "../images/whitetextbg2.png";
-import Whitetextbg3 from "../images/whitetextbg3.png";
+import { Carousel } from 'react-bootstrap';
+import whitetextbg from "../images/whitetextbg.png";
+import whitetextbg2 from "../images/whitetextbg2.png";
+import whitetextbg3 from "../images/whitetextbg3.png";
+import whitetextbg4 from "../images/whitetextbg4.png";
+import whitetextbg5 from "../images/whitetextbg5.png";
+
 
 
 const HomePage = props => {
     return (
         <div className="homepage">
-            <h1>I AM</h1>
+            <div>
+                <div className='container-fluid' >
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <h1>I AM</h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <Carousel>
 
-            <h1>JAMES PEREY</h1>
-            <h1>A WEB DEVELOPER</h1>
-            <h1>JAM JAM</h1>
-            <h1>A FULL STACK ENGINEER</h1>
-            <h1>A REVIT DESIGNER</h1>
-            <h1>A MECHANICAL ENGINEER</h1>
+                                <Carousel.Item interval="500">
+                                    <img
+                                        className="d-block w-100"
+                                        src={whitetextbg}
+                                        alt="First slide"
+                                        height="300"
+                                    />
+
+                                </Carousel.Item>
+
+                                <Carousel.Item interval="500">
+                                    <img
+                                        className="d-block w-100"
+                                        src={whitetextbg2}
+                                        alt="Second slide"
+                                        height="300"
+                                    />
+
+                                </Carousel.Item>
+
+                                <Carousel.Item interval="500">
+                                    <img
+                                        className="d-block w-100"
+                                        src={whitetextbg3}
+                                        alt="Third slide"
+                                        height="300"
+                                    />
+
+                                </Carousel.Item>
+
+                                <Carousel.Item interval="500">
+                                    <img
+                                        className="d-block w-100"
+                                        src={whitetextbg4}
+                                        alt="Third slide"
+                                        height="300"
+                                    />
+
+                                </Carousel.Item>
+
+                                <Carousel.Item interval="500">
+                                    <img
+                                        className="d-block w-100"
+                                        src={whitetextbg5}
+                                        alt="Third slide"
+                                        height="300"
+                                    />
+
+                                </Carousel.Item>
+
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
