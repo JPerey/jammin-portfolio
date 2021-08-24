0ec1ebe8090d8556dd9042d81153c1f95ef3ce09
James Perey <jamjam.perey@gmail.com>
2021-08-24 01:55:37 -0700

    WIP: home page looks a ton better now

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
-import Pic1 from "../images/test1.png";
 import jpburger from "../images/jpburger.jpg";
+import jpjumping from "../images/jpjumping.jpg";
+import jpdrink from "../images/jpdrink.jpg";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
@@ -8,45 +9,22 @@ const HomePage = props => {
     let picChoice = props.selPicChoice;
     return (
         <div className="homepage">
-            <div className="profile-pic">
+
+            <Card alignment="center"className="text-center " bg="secondary" text="light" style={{ width: '25rem' }}>
                 {picChoice === 0 &&
-                <img src={Pic1} alt="profile pic 1" width="300" height="300"/>
+                <Card.Img variant="top" src={jpdrink} alt="profile pic 1"/>
                 }
                 {picChoice === 1 &&
-                <img href="#" alt="profile pic 2"/>
+                <Card.Img variant="top" src={jpburger} alt="profile pic 2"/>
                 }
                 {picChoice === 2 &&
-                <img href="#" alt="profile pic 3"/>
-                }
-            </div>
-            <div className="pic-buttons">
-                <p>JP's many faces</p>
-                <button variant="outline-dark" onClick={() => props.setPicChoice(0)}>Happy</button>
-                <button variant="outline-dark" onClick={() => props.setPicChoice(1)}>Scared</button>
-                <button variant="outline-dark" onClick={() => props.setPicChoice(2)}>Confused</button>
-            </div>
-
-            <Card className="text-center" bg="secondary" text="light" >
-                {picChoice === 0 &&
-                <Card.Img variant="top" src={Pic1} alt="profile pic 1"/>
-                }
-                {picChoice === 1 &&
-                <Card.Img variant="top" src={jpburger} alt="profile pic 1"/>
-                }
-                {picChoice === 2 &&
-                <Card.Img variant="top" src={Pic1} />
+                <Card.Img variant="top" src={jpjumping} alt="profile pic 3"/>
                 }
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Button variant="dark">Go somewhere</Button>
-                    <Button variant="dark">Hungry?</Button>
-                    <Button variant="dark">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-
-            <Card className="text-center" bg="secondary" text="light" style={{ width: '80vw' }}>
-                <Card.Header>Bio</Card.Header>
-                <Card.Body>
+                    <Card.Title>Photo Options</Card.Title>
+                    <Button variant="dark" onClick={() => props.setPicChoice(0)}>Thirsty James</Button>
+                    <Button variant="dark" onClick={() => props.setPicChoice(1)}>Generous James</Button>
+                    <Button variant="dark" onClick={() => props.setPicChoice(2)}>Excited James</Button>
                     <Card.Title>A little about me.</Card.Title>
                     <Card.Text>
                     <p>{bioLength === 0 ? "JP gives it his best shot. Always." : ""}</p>
@@ -59,7 +37,6 @@ const HomePage = props => {
                     <Button variant="dark" onClick={() => props.setBioChoice(1)}>Elevator</Button>
                     <Button variant="dark" onClick={() => props.setBioChoice(2)}>Lengthy</Button>
                 </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
             </Card>
         </div>
     )
