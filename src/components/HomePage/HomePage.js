6bde439c22942c3e60e5fa887c360bbecaed2e9e
James Perey <jamjam.perey@gmail.com>
2021-08-24 02:12:40 -0700

    added more words to homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -25,15 +25,15 @@ const HomePage = props => {
                     <Button variant="dark" onClick={() => props.setPicChoice(0)}>Thirsty James</Button>
                     <Button variant="dark" onClick={() => props.setPicChoice(1)}>Generous James</Button>
                     <Button variant="dark" onClick={() => props.setPicChoice(2)}>Excited James</Button>
-                    <Card.Title>A little about me.</Card.Title>
+                    <Card.Title>A little About Me.</Card.Title>
                     <Card.Text>
-                    <p>{bioLength === 0 ? "JP gives it his best shot. Always." : ""}</p>
+                    <p>{bioLength === 0 ? "You can only do your best, and boy am I trying. " : ""}</p>
                     <p>{bioLength === 1 ? "James Perey is an aspiring full stack engineer that has just got his wings from General Assembly." 
                 + " He aims to merge his previous experience as an engineer to develop amazing solutions to those he deems worthy (i.e. whoever will pay him)" : ""}</p>
                 <p>{bioLength === 2 ? "Having built my first computer when he was 17, James Perey is no stranger to complex, yet exciting problems hardware and software provide."
                 +"This interest followed him  to forging a path to full stack engineering, and as such he aptly understands front-end, back-end, and all the inbetween-ends." : ""}</p>
                     </Card.Text>
-                    <Button variant="dark" onClick={() => props.setBioChoice(0)}>short & Sweet</Button>
+                    <Button variant="dark" onClick={() => props.setBioChoice(0)}>Short & Sweet</Button>
                     <Button variant="dark" onClick={() => props.setBioChoice(1)}>Elevator</Button>
                     <Button variant="dark" onClick={() => props.setBioChoice(2)}>Lengthy</Button>
                 </Card.Body>
